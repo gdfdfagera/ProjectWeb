af3a71eccbac3eaf16a795320d3d40de18dea835
Diga <dias.gainatulinsc@gmail.com>
2023-10-27 18:10:10 +0600

    a

--- JAM/js/index.js
+++ JAM/js/index.js
@@ -1,3 +1,12 @@
+let s = prompt("Verify that you are not a robot: My name is...");
+while (true){
+    if (s == "My name is..."){
+        break;
+    }
+    s = prompt("Verify that you are not a robot: My name is...");
+}
+
+
 document.querySelector("#why_but").onclick = function fun(){
     var content = document.getElementById("causess");
     var display = window.getComputedStyle(content).getPropertyValue("display");
