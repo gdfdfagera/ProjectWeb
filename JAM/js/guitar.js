9cbefb691d9f9ce51eb14f2c66de923d0e826717
Diga <dias.gainatulinsc@gmail.com>
2023-10-31 21:15:36 +0600

    as

--- JAM/js/guitar.js
+++ JAM/js/guitar.js
@@ -1,23 +1,14 @@
 const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
-let currentIndex = 0;
 
-function showSlide(index) {
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${100 * (i - index)}%)`;
-  });
+
+function fun1(){
+  slider.style.transform = 'translateX(25%)';
 }
 
-nextButton.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % slides.length;
-  showSlide(currentIndex);
-});
+function fun2(){
+  slider.style.transform = 'translateX(-25%)';
+}
 
-prevButton.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  showSlide(currentIndex);
-});
-
-showSlide(currentIndex);
+function fun3(){
+  slider.style.transform = 'translateX(-75%)';
+}
