05d4bfa83f41d31297ce667829160beed42e1795
Diga <dias.gainatulinsc@gmail.com>
2023-11-01 23:06:56 +0600

    startOfDOM

--- JAM/js/guitar.js
+++ JAM/js/guitar.js
@@ -12,3 +12,31 @@ function fun2(){
 function fun3(){
   slider.style.transform = 'translateX(-75%)';
 }
+
+
+// function fullscreen(){
+//   const fullscreenDiv = document.createElement("div");
+
+//   fullscreenDiv.style.position = "fixed";
+//   fullscreenDiv.style.top = "0";
+//   fullscreenDiv.style.left = "0";
+//   fullscreenDiv.style.width = "100%";
+//   fullscreenDiv.style.height = "100%";
+//   fullscreenDiv.style.backgroundColor = "white"; 
+//   fullscreenDiv.style.display = "flex";
+//   fullscreenDiv.style.justifyContent = "center";
+
+
+//   document.body.appendChild(fullscreenDiv);
+  
+//   const block = document.createElement("div");
+//   block.style.height = "100%";
+//   block.style.width = "60%";
+//   block.style.backgroundColor = "green";
+
+
+//   fullscreenDiv.appendChild(block);
+
+
+//   const advise = document.createElement("div");
+// }
