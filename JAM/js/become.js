2941384875d2d16badef0f3bed17903e77db5c01
Diga <dias.gainatulinsc@gmail.com>
2023-11-04 20:38:56 +0600

    f

--- JAM/js/become.js
+++ JAM/js/become.js
@@ -1,5 +1,9 @@
-const img = ["https://s8.hostingkartinok.com/uploads/images/2018/09/66ee2632b62f5d628b98925ecc1b163f.jpg", "https://cdn.culture.ru/images/319bcc77-dd02-5e4d-a6f5-8bade4d63bdd", "https://kartinkin.net/uploads/posts/2022-12/1670319783_1-kartinkin-net-p-baraban-kartinka-oboi-1.jpg", "https://www.lessonface.com/sites/default/files/user-files/1649/person-playing-wind-instrument-2254140.jpg"];
-const imgText = ["Piano", "Guitar", "Drums", "Flute"];
+const images = {
+    "Piano": "https://s8.hostingkartinok.com/uploads/images/2018/09/66ee2632b62f5d628b98925ecc1b163f.jpg",
+    "Guitar": "https://cdn.culture.ru/images/319bcc77-dd02-5e4d-a6f5-8bade4d63bdd",
+    "Drums": "https://kartinkin.net/uploads/posts/2022-12/1670319783_1-kartinkin-net-p-baraban-kartinka-oboi-1.jpg",
+    "Flute": "https://www.lessonface.com/sites/default/files/user-files/1649/person-playing-wind-instrument-2254140.jpg"
+};
 
 let index = 0;
 
@@ -7,17 +11,56 @@ const sliderImg = document.getElementById("sliderImage");
 const prevBut = document.getElementById("prev");
 const nextBut = document.getElementById("next");
 
-function updateImg(){
-    sliderImg.src = img[index];
-    document.getElementById("text").textContent = imgText[index];
+function updateImg() {
+    const imageNames = Object.keys(images);
+    const currentImageName = imageNames[index];
+    sliderImg.src = images[currentImageName];
+    document.getElementById("text").textContent = currentImageName;
 }
 
-prevBut.addEventListener("click", function(){
-    index = (index - 1 + img.length) % img.length;
+prevBut.addEventListener("click", function () {
+    index = (index - 1 + Object.keys(images).length) % Object.keys(images).length;
     updateImg();
 });
 
-nextBut.addEventListener("click", function(){
-    index = (index + 1) % img.length;
+nextBut.addEventListener("click", function () {
+    index = (index + 1) % Object.keys(images).length;
     updateImg();
+});
+
+var mo1 = document.getElementById('mo1');
+var mo2 = document.getElementById('mo2');
+var mo3 = document.getElementById('mo3');
+var mo4 = document.getElementById('mo4');
+
+mo1.addEventListener("mouseover", function(event){
+    mo1.style.background = "red";
+});
+
+mo1.addEventListener("mouseout", function(event){
+    mo1.style.background = "white";
+});
+
+mo2.addEventListener("mouseover", function(event){
+    mo2.style.background = "red";
+});
+
+mo2.addEventListener("mouseout", function(event){
+    mo2.style.background = "white";
+});
+
+mo3.addEventListener("mouseover", function(event){
+    mo3.style.background = "red";
+});
+
+mo3.addEventListener("mouseout", function(event){
+    mo3.style.background = "white";
+});
+
+mo4.addEventListener("mouseover", function(event){
+    mo4.style.background = "red";
+});
+
+mo4.addEventListener("mouseout", function(event){
+    mo4.style.background = "white";
 });
